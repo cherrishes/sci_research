5a96d3f76486c35c7673c83cf7f3b1a759c41a3c
cherry <rendy@56iq.com>
2018-03-04 18:14:10 +0800

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+# !/bash/bin/env python
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+__author__ = 'rdy'
+
+
+def home(request):
+    return render(request, "home.html", locals())
---
+++ sci_research/settings.py
@@ -0,0 +1,102 @@
+"""
+Django settings for sci_research project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0ta1zfh6_k&#ccok^7(=v1w0@7_i+a*n(k3csbhb#%#eq76=6g'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'sci_research.urls'
+
+WSGI_APPLICATION = 'sci_research.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+# 设置语言
+LANGUAGE_CODE = 'zh-cn'
+# 设置支持的语言
+LANGUAGES = (('zh-cn', u'中文'), ('en', u'English'))
+'''LANGUAGES = (
+    ('zh-cn', _('Chinese')),
+    ('en', _('English')),
+)'''
+LANGUAGES_SUPPORTED = ('en', 'zh-cn',)
+
+# 设置时区
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+# 模板页中静态地址
+STATIC_URL = '/static/'
+
+# 静态资源目录配置
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
