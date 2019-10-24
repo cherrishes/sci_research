d1dc75d77f699484eded26b9e81c9329f87f39a5
RenDengyong <ren.dengyong@h3c.com>
2019-10-24 14:12:04 +0800

    进一步优化改进，各个导航增加总概要页

--- app/views.py
+++ app/views.py
@@ -120,202 +120,25 @@ def member_scholars(request, name):
     t = "member/scholars/" + name + ".html"
     return render(request, t, locals())
 
-#
-# def xueshuchengguo(request):
-#     return render(request, "xueshuchengguo/xueshuchengguo.html", locals())
-#
-#
-# def xueshulunwen(request):
-#     return render(request, "xueshuchengguo/xueshulunwen.html", locals())
-#
-#
-# def liangdiangongzuo(request):
-#     return render(request, "xueshuchengguo/liangdiangongzuo.html", locals())
-#
-#
-# def fengmianjijin(request):
-#     return render(request, "xueshuchengguo/fengmianjijin.html", locals())
-#
-#
-# def zhuzuoyizhu(request):
-#     return render(request, "xueshuchengguo/zhuzuoyizhu.html", locals())
-#
-#
-# def famingzhuanli(request):
-#     return render(request, "xueshuchengguo/famingzhuanli.html", locals())
-#
-#
-# def xueshujiangli(request):
-#     return render(request, "xueshuchengguo/xueshujiangli.html", locals())
-#
-#
-# def daibiaoxinglunwen(request):
-#     return render(request, "xueshuchengguo/daibiaoxinglunwen.html", locals())
-#
-#
-# def lunwen2017(request):
-#     return render(request, "xueshuchengguo/lunwen2017.html", locals())
-#
-#
-# def lunwen2018(request):
-#     return render(request, "xueshuchengguo/lunwen2018.html", locals())
-#
-#
-# def lunwen2016(request):
-#     return render(request, "xueshuchengguo/lunwen2016.html", locals())
-#
-#
-# def xueshuhuodong(request):
-#     return render(request, "xueshuhuodong/xueshuhuodong.html", locals())
-#
-#
-# def job(request):
-#     return render(request, "jobs/job.html", locals())
-#
-#
-# def shiyanshiwenhua(request):
-#     return render(request, "shiyanshiwenhua/shiyanshiwenhua.html", locals())
-#
-#
-# def zhinengzhixing(request):
-#     return render(request, "shiyanshiwenhua/zhinengzhixing.html", locals())
-#
-#
-# def zuijiatuandui(request):
-#     return render(request, "shiyanshiwenhua/zuijiatuandui.html", locals())
-#
-#
-# def tebiegongxian(request):
-#     return render(request, "shiyanshiwenhua/tebiegongxian.html", locals())
-#
-#
-# def quanjiafu(request):
-#     return render(request, "shiyanshiwenhua/quanjiafu.html", locals())
-#
-#
-# def juhuiyuanzu(request):
-#     return render(request, "shiyanshiwenhua/juhuiyuanzu.html", locals())
-#
-#
-# def shenghuojianying(request):
-#     return render(request, "shiyanshiwenhua/shenghuojianying.html", locals())
-#
-#
-# def xiaoyuanfengguang(request):
-#     return render(request, "shiyanshiwenhua/xiaoyuanfengguang.html", locals())
-#
-#
-# def fengmiangushi(request):
-#     return render(request, "shiyanshiwenhua/fengmiangushi.html", locals())
-#
-#
-# def chengyuanfengcai(request):
-#     return render(request, "shiyanshiwenhua/chengyuanfengcai.html", locals())
-#
-#
-# def biyeshifen(request):
-#     return render(request, "shiyanshiwenhua/biyeshifen.html", locals())
-#
-#
-# def libiejiyu(request):
-#     return render(request, "shiyanshiwenhua/libiejiyu.html", locals())
-#
-#
-# def duzhewenzai(request):
-#     return render(request, "shiyanshiwenhua/duzhewenzai.html", locals())
-#
-#
-# def jiaoxuegongzuo(request):
-#     return render(request, "jiaoxuegongzuo/jiaoxuegongzuo.html", locals())
-#
-#
-# def benkeshengdaoshi(request):
-#     return render(request, "jiaoxuegongzuo/benkeshengdaoshi.html", locals())
-#
-#
-# def rengongzhineng(request):
-#     return render(request, "jiaoxuegongzuo/rengongzhineng.html", locals())
-
 
 def more(request):
     english = is_english(request)
     return render(request, "more/more.html", locals())
 
 
+def notice(request):
+    english = is_english(request)
+    return render(request, "more/notice.html", locals())
+
+
+def team(request):
+    english = is_english(request)
+    return render(request, "more/team.html", locals())
+
+
 def mrshen(request):
     english = is_english(request)
     return render(request, "member/teacher/mrshen.html", locals())
-
-
-# def yilikai(request):
-#     return render(request, "member/yilikai/yilikai.html", locals())
-
-
-
-
-#
-# def sunhong(request):
-#     return render(request, "member/postgraduate/sunhong.html", locals())
-#
-#
-# def zhangsai(request):
-#     return render(request, "member/postgraduate/zhangsai.html", locals())
-#
-#
-# def zhoujincheng(request):
-#     return render(request, "member/postgraduate/zhoujincheng.html", locals())
-#
-#
-# def zhouyi(request):
-#     return render(request, "member/postgraduate/zhouyi.html", locals())
-#
-#
-# def d416(request):
-#     return render(request, "news/d416.html", locals())
-#
-#
-# def d1228(request):
-#     return render(request, "news/d1228.html", locals())
-#
-#
-# def d1220(request):
-#     return render(request, "news/d1220.html", locals())
-#
-#
-# def chenkang(request):
-#     return render(request, "member/postgraduate/chenkang.html", locals())
-#
-#
-# def liyong(request):
-#     return render(request, "member/postgraduate/liyong.html", locals())
-#
-#
-# def jinchunlei(request):
-#     return render(request, "member/postgraduate/jinchunlei.html", locals())
-#
-#
-# def abdulazeez(request):
-#     return render(request, "member/scholars/../templates/member/postgraduate/abdulazeez.html", locals())
-#
-#
-# def d722(request):
-#     return render(request, "news/d722.html", locals())
-#
-#
-# def d1125(request):
-#     return render(request, "news/d1125.html", locals())
-#
-#
-# def d419(request):
-#     return render(request, "news/d419.html", locals())
-#
-#
-# def d624(request):
-#     return render(request, "news/d624.html", locals())
-#
-#
-# def d825(request):
-#     return render(request, "news/d825.html", locals())
 
 
 def about(request, uri):
